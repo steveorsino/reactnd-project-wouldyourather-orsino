df2e3054dfa5ed28673fdce861910c007056c04f
Steve <steveorsino@gmail.com>
2018-12-31 13:43:45 -0500

    added some middleware, started ListBox component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { handleInitialData } from '../actions/shared'
+import LoginBox from './LoginBox'
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+
   render() {
     return (
       <div>
         App Start
+        <LoginBox />
       </div>
     );
   }
---
+++ src/components/LoginBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoginBox extends Component {
+  render() {
+    return (
+      <div className='login-box'>
+        LoginBox
+      </div>
+    )
+  }
+}
+
+export default LoginBox;
