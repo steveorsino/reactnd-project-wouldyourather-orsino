602c970f8ab9dead96e33ad8e9ba6d5ccd3aaeb5
Steve <steveorsino@gmail.com>
2018-12-31 15:53:39 -0500

    added logger, worked on ListBox component

--- src/components/LoginBox.js
+++ src/components/LoginBox.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class LoginBox extends Component {
   render() {
+    console.log('USERS = ', this.props.users)
+    const userArr = [];
+    for (let user in this.props.users) {
+      userArr.push([user]);
+      console.log('USERARR: ', userArr)
+    }
+
     return (
       <div className='login-box'>
-        LoginBox
+        <h3>Please Select a User</h3>
+        <select>
+          {userArr.map((user) => (
+            <option value={user}>{user}</option>
+            ))}
+        </select>
       </div>
     )
   }
 }
 
-export default LoginBox;
+function mapStateToProps({users}) {
+  return {
+    users,
+  }
+}
+
+export default connect(mapStateToProps)(LoginBox);
