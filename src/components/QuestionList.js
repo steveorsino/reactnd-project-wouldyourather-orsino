eeecf787b0107b72e84fb33a91096831c5cb20b2
Steve <steveorsino@gmail.com>
2019-01-05 11:04:14 -0500

    Answered and Unanswered now working

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,27 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import QuestionPreview from './QuestionPreview';
 
 class QuestionList extends Component {
+  state = {
+    showAnswered: false,
+  }
+
+  handleAnswered = () => {
+    this.setState({showAnswered: true})
+  }
+  handleUnanswered = () => {
+    this.setState({showAnswered: false})
+  }
   render() {
-    
+    // let answered = [];
+    // let unanswered = [];
     const { users, questions, authedUser } = this.props;
     console.log('Users',Object.values(users));
     console.log('Questions', Object.values(questions));
+
+    
     
     return (
       <div className='question-list-container'>
         <div>
-          {Object.values(questions).map((question) => {
+          <button className='answered-btn' onClick={this.handleUnanswered}>
+            Unanswered
+          </button>
+          <button className='answered-btn' onClick={this.handleAnswered}>
+            Answered
+          </button>
+          {Object.values(questions).filter((question) => {
             return (
-                <QuestionPreview
-                  key={question.id}
-                  id={question.id}
-                  author={users[question.author].name}
-                  className={}
-                  //show={users[authedUser.authedUser].answers}
-                />
+              this.state.showAnswered === Object.keys(users[authedUser.authedUser].answers).includes(question.id)
             )
+            }).map((question) => {
+              return (
+                  <QuestionPreview
+                    key={question.id}
+                    id={question.id}
+                    author={users[question.author].name}
+                    className='unanswered'
+                    show={Object.keys(users[authedUser.authedUser].answers).includes(question.id) }
+                  />
+              )
           })}
         </div>
       </div>
